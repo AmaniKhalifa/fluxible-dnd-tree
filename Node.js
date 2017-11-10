64ca94e3d6f038398c3d83e1f8c7e8f839f1d479
Amani Khalifa <amani.khalifa@ubermetrics-technologies.com>
2017-11-10 16:25:25 +0100

    Adding drop functionality

--- Node.js
+++ Node.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { DragSource } from 'react-dnd'
+import { DragSource, DropTarget } from 'react-dnd'
 import ItemTypes from './ItemTypes'
 
 const style = {
 	// border: '1px dashed gray',
 	padding: '0.5rem 1rem',
 	marginBottom: '.5rem',
-	backgroundColor: 'white',
+	backgroundColor: 'transparent',
 	width: '20rem',
 }
 
 
 const nodeSource = {
-	beginDrag() {
+	beginDrag(props) {
         console.log("Dragging ...");
-		return {}
+		return props;
 	},
-	endDrag() {
-		console.log(" --- Dropped ...");
-		return {}
+    endDrag(props, monitor) {
+		const didDrop = monitor.didDrop();
+        const droppedTo = monitor.getDropResult();
+        // preventing a node to be dropped inside itself
+        if(droppedTo && droppedTo.node.id != props.node.id)
+		      props.addNode(props, droppedTo);
+
 	},
 }
 
+const nodeTarget = {
+	hover(props, monitor) {
+		const { id: draggedId } = monitor.getItem();
+		const { id: overId } = props;
+        // TODO Do some stuff when on hover
 
+	},
+    drop(props, monitor, component) {
+        // monitor.didDrop() checkes if the event was handled by a nested (child) node.
+        if(!monitor.didDrop()){
+            // these (props which is the node that I dropped into) are available to the nodesource as monitor.getDropResult()
+            return props;
+        }
+
+    },
+};
+
+@DropTarget(ItemTypes.NODE, nodeTarget, (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isHovering: monitor.isOver({ shallow: true })
+}))
 @DragSource(ItemTypes.NODE, nodeSource, (connect, monitor) => ({
 	connectDragSource: connect.dragSource(),
 	isDragging: monitor.isDragging(),
@@ -31,21 +55,24 @@ const nodeSource = {
 class Node extends Component {
 	static propTypes = {
 		connectDragSource: PropTypes.func.isRequired,
+        connectDropTarget: PropTypes.func.isRequired,
 		isDragging: PropTypes.bool.isRequired,
         testToggle: PropTypes.func.isRequired,
-        title: PropTypes.string.isRequired,
-        id: PropTypes.number.isRequired,
+        node: PropTypes.any.isRequired,
         children: PropTypes.node,
+        addNode: PropTypes.func.isRequired,
+        isHovering: PropTypes.bool.isRequired
 
 	}
 
 	render() {
-		const { isDragging, connectDragSource, testToggle, title, id, children } = this.props;
+		const { isDragging, connectDragSource, connectDropTarget, testToggle, node, children, isHovering } = this.props;
 		const opacity = isDragging ? 0.4 : 1;
+        const color = isHovering && !isDragging ? 'red' : 'black';
 
-		return connectDragSource(
+		return connectDragSource(connectDropTarget(
             <li
-                key={id}
+                key={node.id}
                 style={{
                     ...style,
                     opacity,
@@ -57,13 +84,13 @@ class Node extends Component {
                     type="checkbox"
                     onChange={testToggle}
                 />
-                {title}
+                <small  style={{color}}> {node.title + " -- ID " + node.id} </small>
                 <ul  style={{ listStyleType: 'none'}}>
                     {children}
                 </ul>
 
             </li>,
-		)
+		));
 	}
 }
 
@@ -71,8 +98,8 @@ export default class StatefulNode extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			title: props.title,
-            id: props.id,
+			node: props.node,
+            addNode: props.addNode
 		}
 	}
 
@@ -80,8 +107,7 @@ export default class StatefulNode extends Component {
 		return (
 			<Node
 				{...this.props}
-				title={this.state.title}
-                id={this.state.id}
+				node={this.state.node}
 				testToggle={() => this.handleToggle()}
 			/>
 		)
--- Tree.js
+++ Tree.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { DragDropContext } from 'react-dnd'
-import HTML5Backend from 'react-dnd-html5-backend'
-import Node from './Node'
+import React, { Component } from 'react';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import Node from './Node';
 
 @DragDropContext(HTML5Backend)
 export default class Tree extends Component {
@@ -22,13 +22,50 @@ export default class Tree extends Component {
         ]
     };
 
+
+    removeNode = (array, id) => {
+        for (var i = 0; i < array.length; ++i) {
+            var obj = array[i];
+            if (obj.id === id) {
+                array.splice(i, 1);
+                return true;
+            }
+            else if (obj.children) {
+                if (this.removeNode(obj.children, id)) {
+                    if (obj.children.length === 0) {
+                        delete obj.children;
+                    }
+                    return true;
+                }
+            }
+        }
+        return;
+    }
+
+    addNode = (node, parent) => {
+        parent = parent.node;
+        node = node.node;
+        // TODO delete the node and add the same node to the same place
+        this.removeNode(this.state.tree, node.id);
+
+        if(!parent.children){
+            parent.children = [];
+        }
+        parent.children.push(node);
+        console.log("Tree  ", this.state.tree);
+        this.forceUpdate();
+
+    };
+
+
+
 	render() {
         const buildNode = (node) => {
             var children = [];
             for (var i = 0; node.children && i < node.children.length; i++) {
                   children.push(buildNode(node.children[i]));
             }
-            return <Node id={node.id} title={node.title} > {node.children && children } </Node>;
+            return <Node addNode={this.addNode} node={node} > {node.children && children } </Node>;
         };
 
         let nodes = [];
@@ -39,7 +76,7 @@ export default class Tree extends Component {
 		return (
 			<div style={{ overflow: 'hidden', clear: 'both', margin: '-.5rem' }}>
 				<div style={{ float: 'left' }}>
-                    <ul  style={{ listStyleType: 'none'}}>
+                    <ul style={{ listStyleType: 'none'}}>
                         {nodes}
                     </ul>
 				</div>
